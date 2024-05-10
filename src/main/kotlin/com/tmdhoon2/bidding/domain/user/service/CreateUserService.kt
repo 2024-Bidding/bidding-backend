b10901eeae9376b37da8bf16423402ccb3d1d128
Tmdhoon2 <hks026naver@gmail.com>
2024-05-10 09:57:33 +0900

    refactor :: 유저 생성 시 default profile image 설정

--- src/main/kotlin/com/tmdhoon2/bidding/domain/user/service/CreateUserService.kt
+++ src/main/kotlin/com/tmdhoon2/bidding/domain/user/service/CreateUserService.kt
@@ -24,6 +24,7 @@ class CreateUserService(
                     email = email,
                     name = name,
                     password = passwordEncoder.encode(request.password),
+                    profileImageUrl = profileImageUrl ?: "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg",
                 )
             },
         )
