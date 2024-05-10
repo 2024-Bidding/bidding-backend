3405b9aedebac4972e5a1e8835682bd3221c0c98
Tmdhoon2 <hks026naver@gmail.com>
2024-05-10 10:03:08 +0900

    refactor :: user notfound 예외 처리

--- src/main/kotlin/com/tmdhoon2/bidding/global/security/auth/JwtUserDetailsService.kt
+++ src/main/kotlin/com/tmdhoon2/bidding/global/security/auth/JwtUserDetailsService.kt
@@ -3,6 +3,7 @@ package com.tmdhoon2.bidding.global.security.auth
 import com.gil.easyjwt.user.JwtUser
 import com.gil.easyjwt.user.QueryJwtUserService
 import com.tmdhoon2.bidding.domain.user.entity.repository.UserRepository
+import com.tmdhoon2.bidding.global.exception.NotFoundException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import java.util.*
@@ -12,11 +13,7 @@ class JwtUserDetailsService(
     private val userRepository: UserRepository,
 ) : QueryJwtUserService {
     override fun execute(id: String): Optional<JwtUser> {
-        val user = userRepository.findByIdOrNull(id.toLong())
-        return if (user == null) {
-            Optional.empty()
-        } else {
-            Optional.of(user as JwtUser)
-        }
+        val user = userRepository.findByIdOrNull(id.toLong()) ?: throw NotFoundException("User Not Found")
+        return Optional.of(user as JwtUser)
     }
 }
