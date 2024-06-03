d273d5b58f37e2d09b12dfae173351645e806d04
Tmdhoon2 <hks026naver@gmail.com>
2024-06-03 16:16:47 +0900

    refactor :: 물품 조회 기능 리팩토링

--- src/main/kotlin/com/tmdhoon2/bidding/domain/item/service/QueryItemsService.kt
+++ src/main/kotlin/com/tmdhoon2/bidding/domain/item/service/QueryItemsService.kt
@@ -2,6 +2,7 @@ package com.tmdhoon2.bidding.domain.item.service
 
 import com.tmdhoon2.bidding.domain.item.controller.dto.response.ItemsResponse
 import com.tmdhoon2.bidding.domain.item.controller.dto.response.toItemsResponse
+import com.tmdhoon2.bidding.domain.item.entity.BiddingStatus
 import com.tmdhoon2.bidding.domain.item.entity.repository.ItemsRepository
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
@@ -13,6 +14,6 @@ class QueryItemsService(
     @Transactional
     fun execute(): ItemsResponse {
         val items = itemsRepository.findAll()
-        return items.toItemsResponse()
+        return items.filter { it.biddingStatus == BiddingStatus.IN_BIDDING }.toItemsResponse()
     }
 }
